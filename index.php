0eae7dc7c202572d9ee86265aa6ad481ba5651a5
Neeiser <stortini.study@gmail.com>
2022-04-08 17:14:02 +0200

    snack 2 structure

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
     <title>Snack PHP 1</title>
 </head>
 <body>
-    <section class="snack-first" style="text-align: center;">
+
+    <!-- SNACK 1 -->
+    <section id="snack-1st" style="text-align: center;">
     <h1>Snack 1</h1>
         <?php
             $array_partita= [
@@ -22,7 +24,7 @@
                     ],
                 ],
             ];
-
+            
             for ($i = 0; $i < count($array_partita); $i++) {?>
                 
                 <span><?= $array_partita[$i]['squadre']['casa'] ?></span>
@@ -35,14 +37,27 @@
         
         <hr>
     </section>
+
+    <!-- SNACK 2 -->
+    <section id="snack-2nd">
+        <?php
+            $name = $_GET['name'];
+            $mail = $_GET['mail'];
+            $age = $_GET['age'];
+        ?>
+            <form action="" method="get">
+                Name: <input style="display: block;" type="text" name="name" id="name">
+                Mail: <input style="display: block;" type="text" name="mail" id="mail">
+                Age: <input style="display: block;" type="text" name="age" id="age">
+                <input type="submit">
+            </form>
+
+    </section>
     
 </body>
 </html>
 
-<!-- 
-Snack 1
-Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
-Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
-Stampiamo a schermo tutte le partite con questo schema.
-Olimpia Milano - Cantù | 55-60 
--->
+<!-- Snack 2
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) 
+che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. 
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
